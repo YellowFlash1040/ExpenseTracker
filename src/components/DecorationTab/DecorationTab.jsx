bef7c7cc06a7d357638b762ad9be71e201260088
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-02-18 21:04:30 +0100

    I made decoration tab more smooth
    But to do that I had to give up balance and percentage values changing. Because what I did was just deleting MoveDecorationTab wrapper and adding css animation to the decorationTab div. But now it's impossible to follow it's position inside container and therefore change balance and percentage values when it's needed. But in the technical task there was nothing about that, so I thought that it will be a better solution for now

--- src/components/DecorationTab/DecorationTab.jsx
+++ src/components/DecorationTab/DecorationTab.jsx
@@ -1,35 +1,21 @@
-import React from "react"
 import styles from "./DecorationTab.module.css"
-import MoveDecorationTab from "./MoveDecorationTab"
 import ArrowTopRight from "@/assets/icons/ArrowTopRight.svg?react"
 
 export const DecorationTab = () => {
   return (
-    <MoveDecorationTab>
-      {(position, balance, percentage) => (
-        <div
-          style={{
-            position: "absolute",
-            top: `${position.top}px`,
-            left: `${position.left}px`,
-          }}
-        >
-          <div className={styles.decorationTab}>
-            <div className={styles.arrowWrapper}>
-              <ArrowTopRight className={styles.arrowTopRight} />
-            </div>
-            <div>
-              <h1 className={styles.text}>Your balance</h1>
-              <ul className={styles.incomeList}>
-                <li className={styles.income}>&#36;{balance}</li>
-                <li className={styles.icomePercentage}>
-                  &#43;{percentage}&#37;
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      )}
-    </MoveDecorationTab>
+    <div className={styles.decorationTab}>
+      <div className={styles.arrowWrapper}>
+        <ArrowTopRight className={styles.arrowTopRight} />
+      </div>
+      <div>
+        <h1 className={styles.text}>Your balance</h1>
+        <ul className={styles.incomeList}>
+          <li className={styles.income}>&#36;{/* balance */ 671.429}</li>
+          <li className={styles.icomePercentage}>
+            &#43;{/* percentage */ 25.38}&#37;
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
