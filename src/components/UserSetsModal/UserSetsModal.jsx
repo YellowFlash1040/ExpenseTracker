a87a9ddaa3ce2ed2eb57ac7436880fac568cdc39
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-02-19 21:18:18 +0100

    I fixed bug with adding new transaction from transaction form and bublik

--- src/components/UserSetsModal/UserSetsModal.jsx
+++ src/components/UserSetsModal/UserSetsModal.jsx
@@ -50,7 +50,7 @@ export const UserSetsModal = ({ closeModal }) => {
     try {
       const avatarArr = userAvatar.split("avatar/")
       const avatarId = avatarArr[1].split(".")[0]
-      console.log(avatarId)
+      // console.log(avatarId)
       await dispatch(deleteUsersAvatarThunk(avatarId))
       setAvatar(null)
     } catch (e) {
--- src/components/WidgetContainer/WidgetContainer.jsx
+++ src/components/WidgetContainer/WidgetContainer.jsx
@@ -6,7 +6,7 @@ export const WidgetContainer = ({ className, children, moneyDeal }) => {
   const navigate = useNavigate()
 
   const handle = () => {
-    console.log(moneyDeal);
+    // console.log(moneyDeal);
     if (moneyDeal === "expenses" || moneyDeal === "incomes")
       navigate(`/transactions/${moneyDeal}`)
   }
